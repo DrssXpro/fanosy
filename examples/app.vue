16cf315fb4ed6094c4f35bf346ee41d09519533a
Async <1827951482@qq.com>
2023-08-22 14:44:04 +0800

    feat: 增加组件样例板块

---
+++ examples/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test-container">组件测试</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.test-container {
+  margin: 0 auto;
+  width: 90vw;
+  height: 90vh;
+  border: 1px solid red;
+  box-sizing: border-box;
+  padding: 10px;
+}
+</style>
